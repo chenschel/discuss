5f58ddb0f66f41e70716f0970d61190592569e55
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-02-09 19:53:06 +0100

    edit and update a topic. edit link in list for editing topic and get back link without editing.

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -18,14 +18,38 @@ defmodule Discuss.TopicController do
   def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
     case Repo.insert(changeset) do
-      {:ok, post} ->
+      {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic has been created")
         |> redirect(to: topic_path(conn, :index))
       {:error, changeset} ->
         conn
         |> put_flash(:error, "Topic has not been created.")
-        |> render "new.html", changeset: changeset
+        |> render("new.html", changeset: changeset)
+    end
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    topic_entity = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic_entity, topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic has been updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, "Topic has not been updated")
+        |> render("edit.html", changeset: changeset, topic: topic_entity)
+
     end
   end
 end
