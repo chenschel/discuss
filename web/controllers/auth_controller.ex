f5fc9abe2da7aa53aad6ee0dc2d2b032ca7cce1e
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-02-17 21:38:45 +0100

    add user id to topics by migration

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,19 +1,25 @@
 defmodule Discuss.AuthController do
   use Discuss.Web, :controller
-  plug Ueberauth
+  plug(Ueberauth)
 
   alias Discuss.User
 
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
-    user_params = %{email: auth.info.email, token: auth.credentials.token, provider: Atom.to_string(auth.provider)}
+    user_params = %{
+      email: auth.info.email,
+      token: auth.credentials.token,
+      provider: Atom.to_string(auth.provider)
+    }
+
     changeset = User.changeset(%User{}, user_params)
 
     signin(conn, changeset)
   end
 
   def signout(conn, _params) do
+    # put_session(:user_id, nil) will work, but it maby does not delete all collected user data so the whole session will be deleted here
     conn
-    |> configure_session(drop: true) # put_session(:user_id, nil) will work, but it maby does not delete all collected user data so the whole session will be deleted here
+    |> configure_session(drop: true)
     |> put_flash(:info, "Successfully signed out")
     |> redirect(to: topic_path(conn, :index))
   end
@@ -25,6 +31,7 @@ defmodule Discuss.AuthController do
         |> put_flash(:info, "Welcome back")
         |> put_session(:user_id, user.id)
         |> redirect(to: topic_path(conn, :index))
+
       {:error, _reason} ->
         conn
         |> put_flash(:error, "Error singing in")
@@ -36,6 +43,7 @@ defmodule Discuss.AuthController do
     case Repo.get_by(User, email: changeset.changes.email) do
       nil ->
         Repo.insert(changeset)
+
       user ->
         {:ok, user}
     end
