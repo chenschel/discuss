fe531d9865190f7f985f8689f638c5ca08bae710
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-02-17 21:24:18 +0100

    add restriction by require auth plug

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,25 +3,29 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+  plug(Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete])
+
   def index(conn, _params) do
     topics = Repo.all(Topic)
 
-    render conn, "index.html", topics: topics
+    render(conn, "index.html", topics: topics)
   end
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
-    render conn, "new.html", changeset: changeset
+    render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
+
     case Repo.insert(changeset) do
       {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic has been created")
         |> redirect(to: topic_path(conn, :index))
+
       {:error, changeset} ->
         conn
         |> put_flash(:error, "Topic has not been created.")
@@ -33,7 +37,7 @@ defmodule Discuss.TopicController do
     topic = Repo.get(Topic, topic_id)
     changeset = Topic.changeset(topic)
 
-    render conn, "edit.html", changeset: changeset, topic: topic
+    render(conn, "edit.html", changeset: changeset, topic: topic)
   end
 
   def update(conn, %{"id" => topic_id, "topic" => topic}) do
@@ -45,16 +49,16 @@ defmodule Discuss.TopicController do
         conn
         |> put_flash(:info, "Topic has been updated")
         |> redirect(to: topic_path(conn, :index))
+
       {:error, changeset} ->
         conn
         |> put_flash(:error, "Topic has not been updated")
         |> render("edit.html", changeset: changeset, topic: topic_entity)
-
     end
   end
 
   def delete(conn, %{"id" => topic_id}) do
-    Repo.get!(Topic, topic_id) |> Repo.delete!
+    Repo.get!(Topic, topic_id) |> Repo.delete!()
 
     conn
     |> put_flash(:info, "Topic has been deleted")
